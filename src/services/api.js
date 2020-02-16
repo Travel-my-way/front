c2f31bcc1733064669d780d08c495c94bc936803
Jérôme Cognet <jerome.cognet@link-value.fr>
2020-02-16 14:15:53 +0100

    MEP : update code for heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,7 @@ import Axios from 'axios'
 import fakeJourney from '../../fakeJourney'
 
 export const api = Axios.create({
-  // baseURL: apiConfig.backendUrl,
-  // ⬇️ @TODO placer url dans un .env
-  baseURL: 'http://127.0.0.1:5000'
+  baseURL: API_URL
 })
 
 export const getJourney = (departureLat, departureLng, arrivalLat, arrivalLng, date) => 
