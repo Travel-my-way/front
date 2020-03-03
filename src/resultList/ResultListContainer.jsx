56fe326bd51a40aec556f0949a10030deb280b90
sophie.michaudel@gmail.com <sophie.michaudel@gmail.com>
2020-03-03 19:10:33 +0100

    add loader and clean jsx

--- src/resultList/ResultListContainer.jsx
+++ src/resultList/ResultListContainer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import ResultCard from './ResultCard'
 import Header from '../components/Header'
 import SearchContainer from '../search/SearchContainer'
+import loader from '../img/svg/loader.svg'
 
 const sortByCO2 = results => {
   return results.sort((a, b) => a.total_gCO2 - b.total_gCO2)
@@ -27,28 +28,39 @@ const addEcoComparisonToJourney = journeys => {
   })
 }
 
-const ResultListContainer = ({ results, setResults }) => {
+const renderJourneysList = results => {
   const { journeys, isLoading } = results
   const noResult = journeys === undefined || journeys.length === 0
   const sortedJourneys = noResult ? [] : addEcoComparisonToJourney(sortByCO2(journeys))
+  if (isLoading) {
+    return (
+      <div className="icn-loader">
+        <img className="icn-loader-img" src={loader} alt="loader" />
+        <p>Calcul des itinéraires</p>
+      </div>
+    )
+  }
+  if (noResult && !isLoading) {
+    return <p>Désolé, votre recherche n'a abouti à aucun résultat</p>
+  }
+  return (
+    <div>
+      <h2>Travel my Way vous recommande</h2>
+      {sortedJourneys.map(journey => (
+        <Link key={journey.id} to={`/results/${journey.id}`}>
+          <ResultCard journey={journey} />
+        </Link>
+      ))}
+    </div>
+  )
+}
 
+const ResultListContainer = ({ results, setResults }) => {
   return (
     <div className="page-results">
       <Header />
       <SearchContainer setResults={setResults} results={results} />
-      <main className="content-wrapper">
-        {isLoading && <p>Chargement...</p>}
-        {noResult && !isLoading ? (
-          <p>Désolé, votre recherche n'a abouti à aucun résultat</p>
-        ) : (
-          <h2>Travel my Way vous recommande</h2>
-        )}
-        {sortedJourneys.map(journey => (
-          <Link key={journey.id} to={`/results/${journey.id}`}>
-            <ResultCard journey={journey} />
-          </Link>
-        ))}
-      </main>
+      <main className="content-wrapper">{renderJourneysList(results)}</main>
     </div>
   )
 }
--- src/search/SearchContainer.jsx
+++ src/search/SearchContainer.jsx
@@ -41,26 +41,18 @@ const SearchContainer = ({ setResults, results }) => {
         <div className="select-values">
           <select name="number-travelers">
             <option value="">1 voyageur</option>
-            <option value="2">2 voyageurs</option>
-            <option value="3">3 voyageurs</option>
-            <option value="4">4 voyageurs</option>
-            <option value="5">5 voyageurs</option>
-            <option value="6">6 voyageurs</option>
-            <option value="7+">7 voyageurs ou plus</option>
           </select>
         </div>
 
         <div className="select-values">
           <select name="type-travel">
-            <option value="both">Aller-retour</option>
             <option value="go">Aller simple</option>
-            <option value="back">Retour simple</option>
           </select>
         </div>
       </div>
 
       <div className="searchbar_bottom">
-        <form action="" method="post" className="flex space-between">
+        <form method="post" className="flex space-between">
           <AutocompleteAddress
             placeholder="9 rue d'Alexandrie, 75002 Paris"
             changeAddress={changeDepartureAddress}
