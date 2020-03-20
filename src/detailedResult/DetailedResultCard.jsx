40acc0a6b72f768f4a219b91f7703ef7cf1b035a
sophie.michaudel@gmail.com <sophie.michaudel@gmail.com>
2020-03-20 16:57:30 +0100

    add total in co2

--- src/detailedResult/DetailedResultCard.jsx
+++ src/detailedResult/DetailedResultCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import StepsCard from './StepsCard'
+import { getCO2InKg } from '../journey.utils'
 
 const DetailedResultCard = ({ selectedJourney }) => {
+  const totalCO2InKg = getCO2InKg(selectedJourney.total_gCO2)
+
   return (
     <div className="detailed-result-card">
+      <h3 className="ecology">{totalCO2InKg} de CO2 émis</h3>
       <StepsCard steps={selectedJourney.journey_steps} />
       {selectedJourney.booking_link && (
         <a className="button primary" href={selectedJourney.booking_link} target="_blank">
